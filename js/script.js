bbce32b98f988342e0b98d475678f29d0afd1147
zagray74 <75271911+zagray74@users.noreply.github.com>
2022-01-05 14:11:22 +0200

    Pizza

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+  //var mixer = mixitup('.tabs');
+  mixitup('.bur');
+
+  $('.slider').slick({
+
+  });
+  $(".li")[0].click();
+});
+$(document).ready(function () {
+  $('.menu-burrg').click(function (event) {
+    $('.menu-burrg, .nav').toggleClass('active')
+  });
+});
